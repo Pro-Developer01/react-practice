811c1a8c561221a4070cae64386fba3e83c2d9dc
Pro-Developer01 <yadavshashank70@gmail.com>
2022-12-28 19:06:29 +0530

    Crausel Ui +data prep

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import './App.css'
+import Carausel from './Components/Crausel/Carausel'
 
 export default function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Carausel/>
   )
 }
---
+++ src/Components/Crausel/Carausel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Crausel.css'
+const data=[
+    {url: 'https://img.freepik.com/free-photo/rear-view-programmer-working-all-night-long_1098-18697.jpg?w=740&t=st=1672232078~exp=1672232678~hmac=b361c01c3322bdb85058c31ac4d7a8e51ed7ffa4d43438083315fec1ac7f528b'},
+    {url: 'https://img.freepik.com/free-photo/beautiful-curly-female-freelancer-with-cute-manicure-using-laptop-smiling-indoor-portrait-blonde-secretary-sitting-beside-african-coworker-blue-shirt_197531-3713.jpg?w=740&t=st=1672232162~exp=1672232762~hmac=378b862ef166eeeecfca40f7d9f627a60d8ea7f908b092cac7feaf11c493e316'},
+    {url: 'https://img.freepik.com/free-photo/inspired-african-lady-dancing-with-eyes-closed-indoor-portrait-relaxed-black-girl-isolated_197531-10133.jpg?w=740&t=st=1672232178~exp=1672232778~hmac=d329278708e091fff75be030977ace11c306d17dd63dfb21143d6888ff7c6d62'},
+    {url: 'https://img.freepik.com/free-photo/surprised-dark-haired-girl-posing-with-phone-well-dressed-young-lady-beret-expressing-amazement_197531-20410.jpg?w=740&t=st=1672232193~exp=1672232793~hmac=564fb1d983fcbf5dd5585575a9f7aaaf962ea881d47d1933836caae5e08c7d04'},
+]
+
+export default function Carausel() {
+  return (
+    <>
+    <div className="conatiner">
+
+      <div className="parentConatainer">
+        <div className="sliderButtonPrev">
+          <span> &lt;</span>
+        </div>
+        <div className="slider">
+            <img className='slides' src="https://img.freepik.com/free-photo/surprised-dark-haired-girl-posing-with-phone-well-dressed-young-lady-beret-expressing-amazement_197531-20410.jpg?w=740&t=st=1672232193~exp=1672232793~hmac=564fb1d983fcbf5dd5585575a9f7aaaf962ea881d47d1933836caae5e08c7d04" alt="image1" />
+        </div>
+        <div className="sliderButtonNext">
+        <span>&gt;</span>
+        </div>
+      </div>
+        <div className="dots">
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </div>
+    </div>
+    </>
+    
+  )
+}
